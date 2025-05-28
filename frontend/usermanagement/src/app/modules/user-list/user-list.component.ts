25ea45d4cbb6d6c68593d41fd953f5e65c6f3cd2
vliam0602 <v.trclam@gmail.com>
2025-05-29 00:26:31 +0700

    add fe pagination, table actions

--- frontend/usermanagement/src/app/modules/user-list/user-list.component.ts
+++ frontend/usermanagement/src/app/modules/user-list/user-list.component.ts
@@ -1,32 +1,66 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { User } from '../../shared/models/user';
 import { MatTableModule } from '@angular/material/table'
 import { UserApiService } from '../../services/user-api.service';
 import { PagedList } from '../../shared/models/pagedList';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [
+    MatTableModule, 
+    MatPaginatorModule, 
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
+  ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
   private userApi = inject(UserApiService);
 
-  users: PagedList<User> =
-  {
-    items:[],
-    totalCount: 0,
-    pageIndex: 1,
-    pageSize: 5
-  };
-  
-  displayedColumns: string[] = ['no', 'username', 'email', 'status', 'joinDate'];
+  pagedUsers: PagedList<User> =
+    {
+      items: [],
+      totalCount: 0,
+      pageIndex: 1,
+      pageSize: 2
+    };
+
+  displayedColumns: string[] = ['no', 'username', 'email', 'status', 'joinDate', 'actions'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit() {
-    this.userApi.getUsers().subscribe(data => {
-      this.users = data;
+    this.loadUser(this.pagedUsers.pageIndex, this.pagedUsers.pageSize);
+  }
+
+  ngAfterViewInit() {
+    this.paginator.page.subscribe((event: PageEvent) => {
+      this.loadUser(event.pageIndex + 1, event.pageSize);
     });
+  }
+
+  loadUser(pageIndex: number, pageSize: number) {
+    this.userApi.getUsers({ pageIndex, pageSize }).subscribe(data =>
+      this.pagedUsers = data
+    );
+  }
+
+  onAddUser() {
+    alert('Add new user');
+  }
+  onEditUser(user: User) {
+    alert('Edit user: '+ user.username);
+  }
+  onDeleteUser(user: User) {
+    alert('Delete user: '+ user.username);
   }
 }
--- frontend/usermanagement/src/app/services/user-api.service.ts
+++ frontend/usermanagement/src/app/services/user-api.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from "@angular/core";
 import { ApiService } from "./api.service";
 import { User } from "../shared/models/user";
 import { PagedList } from "../shared/models/pagedList";
+import { HttpParams } from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +10,13 @@ import { PagedList } from "../shared/models/pagedList";
 export class UserApiService {
     private api = inject(ApiService);
 
-    getUsers() {
-        return this.api.get<PagedList<User>>('api/users');
+    getUsers(params?: {pageIndex: number, pageSize: number}) {
+        let httpParams = new HttpParams();
+        if (params) {
+            httpParams = httpParams
+                .set('pageIndex', params.pageIndex)                
+                .set('pageSize', params.pageSize)                
+        }
+        return this.api.get<PagedList<User>>('api/users', httpParams);
     }
 }
